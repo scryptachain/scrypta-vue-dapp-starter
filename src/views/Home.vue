f7d7df65adfd07da24134db3dcafc39109f10cac
turinglabsorg <turinglabs@criptext.com>
2019-10-25 11:58:11 +0200

    min fixes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,9 +3,11 @@
     <img style="margin: 20px 0" src="../assets/logo.png"><br>
     <h1>Scrypta dApp Starter</h1>
     <p>
-      This is a VueJS starter for Scrypta. You will have all methods under app.scrypta.<br>
-      The starter will connect automatically to one of our random nodes.<br>
-      You will see it in the bottom right corner.
+      This is a VueJS starter for Scrypta dApps.<br><br>
+      You will have all <a href="https://wiki.scryptachain.org/developers/scrypta-core/introduction" target="_blank">ScryptaCore</a> methods under <b>app.scrypta</b><br>
+      The starter will connect automatically to one of our IdaNodes.<br>
+      You will see it in the bottom right corner.<br><br>
+      <a href="https://wiki.scryptachain.org/developers/ida-nodes" target="_blank">Read the docs</a> in our wiki website.
     </p>
     <div class="node-badge" v-if="connected">{{ connected }}</div>
   </div>
@@ -26,24 +28,21 @@ export default {
     this.checkIdaNodes()
   },
   methods: {
-      checkIdaNodes(){
+      async checkIdaNodes(){
         var checknodes = this.scrypta.returnNodes()
         const app = this
         for(var i = 0; i < checknodes.length; i++){
-          this.axios.get('https://' + checknodes[i] + '/check')
-          .then(function (response) {
-             app.nodes.push(response.data.name)
-             if(i == checknodes.length){
-               app.connectToNode()
-             }
-          });
+          let check = await this.axios.get(checknodes[i] + '/wallet/getinfo')
+          if(check.data.blocks !== undefined){
+             app.nodes.push(checknodes[i])
+          }
         }
+        app.connectToNode()
       },
       connectToNode(){
         var app = this
         if(app.connected == ''){
           app.connected = app.nodes[Math.floor(Math.random()*app.nodes.length)];
-          app.checkBalance()
         }
       }
   },
